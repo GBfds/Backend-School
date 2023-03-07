20eff802863f4cf826370ffa2857e1dbd786f16f
GBfds <95057840+GBfds@users.noreply.github.com>
2023-03-07 11:43:00 -0300

    gerando matricula

--- src/controllers/aluno/CreateAlunoController.ts
+++ src/controllers/aluno/CreateAlunoController.ts
@@ -4,7 +4,13 @@ import { CreateAlunoService } from "../../services/aluno/CreateAlunoService";
 class CreateAlunoController{
     async hundle(req: Request, res: Response){
         const {nome, telefone, email, cpf} = req.body
-        const matricula = "876181"
+        const split = cpf.split("",4)
+        
+        while (split.length < 8){
+            let ramdon = Math.floor(Math.random() * 9)
+            split.push(ramdon)
+        }
+        const matricula = split.join("")
 
         const createAlunoService = new CreateAlunoService();
         const newAluno = await createAlunoService.execute({
